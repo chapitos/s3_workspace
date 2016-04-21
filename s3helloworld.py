446203b7dc80a271f2ec86e6bebb5b6a786f29fc
root <root@ip-172-31-30-144.eu-central-1.compute.internal>
2016-04-21 14:16:51 +0000

    Implemented change enabling creation of a test bucket.

--- s3helloworld.py
+++ s3helloworld.py
@@ -1,6 +1,19 @@
 import boto3
+import botocore
 
+BUCKET_NAME = 'mpa-bucket-1'
+print('First S3 Example')
 s3 = boto3.resource('s3')
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+#bucket_num = s3.buckets.all.size
+#print(bucket_num)
+
+#for bucket in s3.buckets.all():
+#    print(bucket.name)
+if not s3.Bucket(BUCKET_NAME) in s3.buckets.all():
+	print('Creating bucket with name: ' + BUCKET_NAME)
+	s3.create_bucket(Bucket=BUCKET_NAME)
+else:
+	print('Bucket ' + BUCKET_NAME + 'already exists!')
+
+
